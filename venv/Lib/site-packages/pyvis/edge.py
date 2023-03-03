9c518e734091e0a4ba84fb1bc17583eb832b8229
Yusuf Ganiyu <airscholar@gmail.com>
2023-03-03 17:50:51 +0000

    updated codebase implementation. fixed bug (#3)
    * Added graph statistics and shortest path analysis
    
    * Restructured codebase. added rail modularised implementation
    
    * updated implementation
    
    * updated codebase

--- venv/Lib/site-packages/pyvis/edge.py
+++
@@ -1,9 +0,0 @@
-class Edge(object):
-
-    def __init__(self, source, dest, directed=False, **options):
-        self.options = options
-        self.options['from'] = source
-        self.options['to'] = dest
-        if directed:
-        	if 'arrows' not in self.options:
-        		self.options["arrows"] = "to"
