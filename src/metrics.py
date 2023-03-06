7f2938bdab6af4801d0fba34888d147e2859b0f1
sferez <simeon.ferez@gmail.com>
2023-03-06 17:00:46 +0000

    restructure code

---
+++ src/metrics.py
@@ -0,0 +1,26 @@
+
+
+def compute_global_metrics(NetworkX_):
+    # return pandas dataframe
+    return 0
+
+def compute_connectivity(NetworkX_):
+    # return pandas dataframe
+    return 0
+
+def compute_degree_centrality(NetworkX_):
+    # return pandas dataframe
+    return 0
+
+def compute_eigen_centrality(NetworkX_):
+    # return pandas dataframe
+    return 0
+
+def compute_closeness_centrality(NetworkX_):
+    # return pandas dataframe
+    return 0
+
+def compute_betweeness_centrality(NetworkX_):
+    # return pandas dataframe
+    return 0
+
---
+++ src/preprocessing.py
@@ -0,0 +1,5 @@
+
+s
+def preprocess(filename_, datatype_):
+    # return NetworkX Object
+    return 0
