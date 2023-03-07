8b8c8787b49c62ce3ed30a3632383c0d53e468fc
VishalAnandV <vl_ad97@ymail.com>
2023-03-07 18:16:45 +0000

    static_on_map and plot_metrics_on_map updated

--- src/visualisation.py
+++ src/visualisation.py
@@ -7,8 +7,25 @@ def dyn_visualisation(NetworkX_):
 #return html file
     return 0
 
-def static_on_map(NetworkX_):
-# return plotly figure
+def static_on_map(Networkx_, title, directed=True):
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    china = world[world['name'] == 'China']
+    china.plot(figsize=(10, 10))
+
+    if directed:
+        pos = nx.get_node_attributes(Networkx_, 'pos')
+        nx.draw(Networkx_, pos, with_labels=False, node_size=3, node_color='red')
+    else:
+        # convert to undirected
+        Networkx_ = Networkx_.to_undirected()
+        pos = nx.get_node_attributes(Networkx_, 'pos')
+        nx.draw(Networkx_, pos, with_labels=False, node_size=3, node_color='red')
+
+    # plot axes
+    plt.axis('on')
+    plt.title(title)
+    plt.show()
+
     return 0
 
 def plot_shortest_distance(NetworkX_, path_):
@@ -20,7 +37,49 @@ def plot_metrics(NetworkX_, dataFrame_, title_):
     return 0
 
 def plot_metrics_on_map(NetworkX_, dataFrame_, title_):
-# return plotly figure
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    china = world[world['name'] == 'China']
+    china.plot(figsize=(10, 10))
+
+    id = input("\nEnter your desired metrics to be displayed:\n1.Degree Centrality \n2.Closeness Centrality \n3.Betweenness Centrality \n4.Eigenvector Centrality \n")
+
+    if id == 1:
+        '''Degree Centrality'''
+        # Calculate the eigen vector centrality for each node
+        eigen_centralities = nx.eigenvector_centrality(NetworkX_)
+
+        # Plot the graph with node size proportional to eigen vector centrality
+        nx.draw(G, with_labels=True, node_size=[v * 1000 for v in eigen_centralities.values()])
+        plt.show()
+
+    elif id == 2:
+        '''Closeness Centrality'''
+        # Calculate the closeness centrality for each node
+        closeness_centralities = nx.closeness_centrality(G)
+
+        # Plot the graph with node size proportional to closeness centrality
+        nx.draw(G, with_labels=True, node_size=[v * 1000 for v in closeness_centralities.values()])
+        plt.show()
+
+    elif id == 3:
+        '''Betweenness Centrality'''
+        # Calculate the betweenness centrality for each node
+        betweenness_centralities = nx.betweenness_centrality(G)
+
+        # Plot the graph with node size proportional to betweenness centrality
+        nx.draw(G, with_labels=True, node_size=[v * 1000 for v in betweenness_centralities.values()])
+        plt.show()
+
+    elif id == 4:
+        '''Closeness Centrality'''
+        # Calculate the closeness centrality for each node
+        closeness_centralities = nx.closeness_centrality(G)
+
+        # Plot the graph with node size proportional to closeness centrality
+        nx.draw(G, with_labels=True, node_size=[v * 1000 for v in closeness_centralities.values()])
+        plt.show()
+
+    # return plotly figure
     return 0
 
 def plot_histogram(NetworkX_, dataFrame_, title_):
