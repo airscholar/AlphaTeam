4235812e5f92efc89ef925e0ab1932f47f20804f
Yusuf Ganiyu <airscholar@gmail.com>
2023-03-07 15:31:41 +0000

    feat: computed the global and node metrics

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,104 @@
+from enum import Enum
+from typing import Type
+import networkx as nx
 
-def preprocess(filename_, datatype_):
-    # return NetworkX Object
+
+class DatasetType(Enum):
+    RAILWAY = 'RAILWAY'
+    CRYPTO = 'CRYPTO'
+    CUSTOM = 'CUSTOM'
+
+
+def preprocess(filename_: str, dataset_type: Type[DatasetType]):
+    switch = {
+        DatasetType.RAILWAY: preprocess_railway,
+        DatasetType.CRYPTO: preprocess_crypto,
+        DatasetType.CUSTOM: preprocess_custom,
+    }
+    func = switch.get(dataset_type, lambda _: None)
+    return func(filename_)
+
+
+def preprocess_railway(filename_: str):
+    network = {}
+    station_id = {}
+    with open(filename_, 'r') as f:
+        prev_train = None
+        prev_station = None
+
+        for line in f:
+            train, st_no, st_id, date, arr_time, dep_time, stay_time, mileage, lat, lon = line.split(",")
+            lat = float(lat)
+            lon = float(lon)
+
+            if train != prev_train:
+                prev_station = None
+                network[train] = []
+
+            station = {
+                "id": int(st_id),
+                "name": f"Station {st_id}",
+                "lat": lat,
+                "lon": lon,
+                "from": prev_station["lat"] if prev_station else None,
+                "to": None,
+            }
+
+            network[train].append(station)
+
+            if prev_station:
+                prev_station["to"] = (lat, lon)
+
+            prev_train = train
+            prev_station = station
+
+    for train in network:
+        for station in network[train]:
+            station_id[(station['lat'], station['lon'])] = station['id']
+
+    return convert_to_DiGraph(create_multi_DiGraph(network, station_id))
+
+
+def create_multi_DiGraph(network, station_id):
+    multi_graph = nx.MultiDiGraph()
+
+    # add nodes to the graph
+    for stations in network:
+        for station in network[stations]:
+            # print(station)
+            # add note with the localisation for visualization
+            multi_graph.add_node(station['id'], pos=(station['lon'], station['lat']))
+
+            from_node = station['id']
+
+            if type(station['to']) is tuple:
+                # if the 'to' value is a tuple, create a new node
+                to_node = station_id[station['to']]
+                multi_graph.add_edge(from_node, to_node)
+            else:
+                continue
+
+    return multi_graph
+
+
+def convert_to_DiGraph(multi_graph):
+    g_directed = nx.DiGraph()
+    for u, v in multi_graph.nodes(data=True):
+        g_directed.add_node(u, pos=v['pos'])
+    for u, v in multi_graph.edges():
+        if g_directed.has_edge(u, v):
+            continue
+        else:
+            g_directed.add_edge(u, v)
+    return g_directed
+
+
+def convert_to_undirected(g_directed):
+    return g_directed.to_undirected()
+
+def preprocess_crypto(filename_: str):
+    return 0
+
+
+def preprocess_custom(filename_: str):
     return 0
--- src/visualisation.py
+++ src/visualisation.py
@@ -1,29 +1,54 @@
+import geopandas as gpd
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 def static_visualisation(NetworkX_):
-# return plotly figure
+    # return plotly figure
     return 0
+
 
 def dyn_visualisation(NetworkX_):
-#return html file
+    # return html file
     return 0
 
-def static_on_map(NetworkX_):
-# return plotly figure
-    return 0
+
+def plot_static_on_map(networkx_, title, directed=True):
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    china = world[world['name'] == 'China']
+    china.plot(figsize=(10, 10))
+
+    if directed:
+        pos = nx.get_node_attributes(networkx_, 'pos')
+        nx.draw(networkx_, pos, with_labels=False, node_size=3, node_color='red')
+    else:
+        # convert to undirected
+        networkx_ = networkx_.to_undirected()
+        pos = nx.get_node_attributes(networkx_, 'pos')
+        nx.draw(networkx_, pos, with_labels=False, node_size=3, node_color='red')
+
+    # plot axes
+    plt.axis('on')
+    plt.title(title)
+
+    return plt
+
 
 def plot_shortest_distance(NetworkX_, path_):
-# return plotly figure
+    # return plotly figure
     return 0
+
 
 def plot_metrics(NetworkX_, dataFrame_, title_):
-# return plotly figure
+    # return plotly figure
     return 0
+
 
 def plot_metrics_on_map(NetworkX_, dataFrame_, title_):
-# return plotly figure
+    # return plotly figure
     return 0
+
 
 def plot_histogram(NetworkX_, dataFrame_, title_):
-# return plotly figure
+    # return plotly figure
     return 0
-
