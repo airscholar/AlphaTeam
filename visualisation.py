993a54cea92c2f1e050ac2a93b978be7e5350551
VishalAnandV <68912423+VishalAnandV@users.noreply.github.com>
2023-03-07 01:33:39 +0000

    Create visualisation.py

---
+++ visualisation.py
@@ -0,0 +1,27 @@
+def static_visualisation(NetworkX_):
+# return plotly figure
+    return 0
+
+def dyn_visualisation(NetworkX_):
+#return html file
+    return 0
+
+def static_on_map(NetworkX_):
+# return plotly figure
+    return 0
+
+def plot_shortest_distance(NetworkX_, path_):
+# return plotly figure
+    return 0
+
+def plot_metrics(NetworkX_, dataFrame_, title_):
+# return plotly figure
+    return 0
+
+def plot_metrics_on_map(NetworkX_, dataFrame_, title_):
+# return plotly figure
+    return 0
+
+def plot_histogram(NetworkX_, dataFrame_, title_):
+# return plotly figure
+    return 0
