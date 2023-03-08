49d217b565fbac3d5540b6c9291ac24389fe7221
VishalAnandV <vl_ad97@ymail.com>
2023-03-08 10:18:00 +0000

    static_on_map and plot_metrics_on_map updated

--- src/visualisation.py
+++ src/visualisation.py
@@ -1,4 +1,6 @@
-
+import geopandas as gpd
+import networkx as nx
+import matplotlib.pyplot as plt
 def static_visualisation(NetworkX_):
 # return plotly figure
     return 0
@@ -41,34 +43,36 @@ def plot_metrics_on_map(NetworkX_, dataFrame_, title_):
     china = world[world['name'] == 'China']
     china.plot(figsize=(10, 10))
 
-    id = input("\nEnter your desired metrics to be displayed:\n1.Degree Centrality \n2.Closeness Centrality \n3.Betweenness Centrality \n4.Eigenvector Centrality \n")
+    id = input("\nEnter your desired metrics to be displayed:"
+               "\n1.Degree Centrality \n2.Closeness Centrality "
+               "\n3.Betweenness Centrality \n4.Eigenvector Centrality \n")
 
     if id == 1:
         '''Degree Centrality'''
         # Calculate the degree centrality for each node
-        degree_centralities = nx.degree_centrality(G)
+        degree_centralities = nx.degree_centrality(NetworkX_)
 
         # Plot the graph with node size proportional to degree centrality
-        nx.draw(G, with_labels=True, node_size=[v * 1000 for v in degree_centralities.values()])
+        nx.draw(NetworkX_, with_labels=True, node_size=[v * 1000 for v in degree_centralities.values()])
         plt.show()
 
 
     elif id == 2:
         '''Closeness Centrality'''
         # Calculate the closeness centrality for each node
-        closeness_centralities = nx.closeness_centrality(G)
+        closeness_centralities = nx.closeness_centrality(NetworkX_)
 
         # Plot the graph with node size proportional to closeness centrality
-        nx.draw(G, with_labels=True, node_size=[v * 1000 for v in closeness_centralities.values()])
+        nx.draw(NetworkX_, with_labels=True, node_size=[v * 1000 for v in closeness_centralities.values()])
         plt.show()
 
     elif id == 3:
         '''Betweenness Centrality'''
         # Calculate the betweenness centrality for each node
-        betweenness_centralities = nx.betweenness_centrality(G)
+        betweenness_centralities = nx.betweenness_centrality(NetworkX_)
 
         # Plot the graph with node size proportional to betweenness centrality
-        nx.draw(G, with_labels=True, node_size=[v * 1000 for v in betweenness_centralities.values()])
+        nx.draw(NetworkX_, with_labels=True, node_size=[v * 1000 for v in betweenness_centralities.values()])
         plt.show()
 
     elif id == 4:
@@ -77,13 +81,66 @@ def plot_metrics_on_map(NetworkX_, dataFrame_, title_):
         eigen_centralities = nx.eigenvector_centrality(NetworkX_)
 
         # Plot the graph with node size proportional to eigen vector centrality
-        nx.draw(G, with_labels=True, node_size=[v * 1000 for v in eigen_centralities.values()])
+        nx.draw(NetworkX_, with_labels=True, node_size=[v * 1000 for v in eigen_centralities.values()])
         plt.show()
 
     # return plotly figure
     return 0
 
 def plot_histogram(NetworkX_, dataFrame_, title_):
-# return plotly figure
+    id = input("\nEnter ID of the below options:"
+               "\n1.Histogram of Longitude \n2.Histogram of Latitude "
+               "\n3.Histogram of Station Number \n4.Histogram of Station ID \n")
+
+
+    if id == 1:
+        column_name = dataFrame_['lon']
+
+        # Create a histogram of the column data
+        plt.hist(column_name)
+        plt.title('Histogram of Longitude')
+        plt.xlabel('Longitude')
+        plt.ylabel('Frequency')
+
+        # Display the plot
+        plt.show()
+
+    elif id == 2:
+        column_name = dataFrame_['lat']
+
+        # Create a histogram of the column data
+        plt.hist(column_name)
+        plt.title('Histogram of Latitude')
+        plt.xlabel('Latitude')
+        plt.ylabel('Frequency')
+
+        # Display the plot
+        plt.show()
+
+    elif id == 3:
+        column_name = dataFrame_['st_no']
+
+        # Create a histogram of the column data
+        plt.hist(column_name)
+        plt.title('Histogram of Station Number')
+        plt.xlabel('Station Number')
+        plt.ylabel('Frequency')
+
+        # Display the plot
+        plt.show()
+
+    elif id == 4:
+        column_name = dataFrame_['st_id']
+
+        # Create a histogram of the column data
+        plt.hist(column_name)
+        plt.title('Histogram of Station ID')
+        plt.xlabel('Station ID')
+        plt.ylabel('Frequency')
+
+        # Display the plot
+        plt.show()
+
+    # return plotly figure
     return 0
 
