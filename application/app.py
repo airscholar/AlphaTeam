0b25bd7f0adfb5c78229efc5074c788d5f2b87ef
Tejas Patil <tejas.coder@gmail.com>
2023-03-10 19:07:07 +0000

    app.py

--- application/app.py
+++ application/app.py
@@ -5,23 +5,33 @@ sys.path.insert(1, '../')
 from src.NetworkGraphs import *
 import scipy as sp
 from flask_caching import Cache
+import matplotlib.pyplot as plt
+import re
+import time
 
 app = Flask(__name__)
-app.secret_key = 'my-secret-key'  # set a secret key for the session
+app.secret_key = 'my-secret-key' # set a secret key for the session
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
 
-filepath = '../datasets/Railway.csv'
-
-cols = ['train', 'st_no', 'st_id', 'date', 'arr_time', 'dep_time', 'stay_time', 'mileage', 'lat', 'lon']
-df = pd.read_csv(filepath, names=cols, header=None)
-networkGraphs = NetworkGraphs(filepath, type="RAILWAY", spatial=True)
-
+# Define the global variable
+networkGraphs = None
 
 @app.route('/')
 def index():
+    # Check if global_metrics is present in the cache and filename is present in the session
+    if cache.has('global_metrics') and 'filename' in session:
+        # Clear the cache
+        cache.clear()
+        # Delete the file
+        filename = session['filename']
+        filepath = './uploads/'+filename
+        if os.path.exists(filepath):
+            os.remove(filepath)
+        imagepath = app.root_path + '/static/img/' + filename + '.png'
+        if os.path.exists(imagepath):
+            os.remove(imagepath)
     return render_template('index.html')
-
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -30,10 +40,11 @@ def upload():
     option = request.form['option']
 
     # Save the CSV file to a folder on the server with a filename based on the selected option and file extension
+    timestamp = str(int(time.time()))
     if option != 'General':
-        filename = option + '.csv'
+        filename = option + re.sub(r'\W+', '', timestamp) + '.csv'
     else:
-        filename = option + '.mtx'
+        filename = option + re.sub(r'\W+', '', timestamp) + '.mtx'
 
     csv_file.save('uploads/' + filename)
     # Do something with the CSV file and selected option
@@ -42,16 +53,19 @@ def upload():
     # Store the filename in a session variable
     session['filename'] = filename
 
+    filepath = './uploads/'+filename
+    # Assign the value to the global variable
+    global networkGraphs
+    networkGraphs = NetworkGraphs(filepath, type=option, spatial=True)
     # Redirect the user to the success page
     return redirect(url_for('home'))
 
-
 @app.route('/home')
-@cache.cached(timeout=3600)  # Cache the result for 1 hour
+@cache.cached(timeout=3600) # Cache the result for 1 hour
 def home():
     # Get the filename from the session variable
     filename = session['filename']
-
+    
     global_metrics = cache.get('global_metrics')
     if global_metrics is None:
         global_metrics = compute_global_metrics(networkGraphs)
@@ -73,20 +87,23 @@ def home():
     # Pass the data to the HTML template
     return render_template('home.html', data=data, table_headers=table_headers, table_rows=table_rows)
 
-
 @app.route('/visualise/static', endpoint='my_static')
 def static():
-    image_path = '/img/example.png'
+    filename = session['filename']
+    obj = plot_static_on_map(networkGraphs, "My Plot", directed=False)
+    plot = plot_static_on_map(networkGraphs, "My Plot", directed=False)
+    image_path = 'img/' + filename + '.png'
+    if not os.path.exists(app.root_path + '/static/img/' + filename + '.png'):
+        plot.savefig(app.root_path + '/static/img/' + filename + '.png', bbox_inches='tight')
+    print(image_path)
     return render_template('static.html', image_path=image_path)
-
 
 @app.route('/visualise/dynamic', endpoint='my_dynamic')
 def dynamic():
     return render_template('dynamic.html')
 
-
 @app.route('/centrality', endpoint='centrality')
-@cache.cached(timeout=3600)  # Cache the result for 1 hour
+@cache.cached(timeout=3600) # Cache the result for 1 hour
 def centrality():
     allCentralityDF = cache.get('allCentralityDF')
     if allCentralityDF is None:
@@ -96,9 +113,8 @@ def centrality():
     table_rows = allCentralityDF.values.tolist()
     return render_template('centrality.html', table_headers=table_headers, table_rows=table_rows)
 
-
 @app.route('/degree', endpoint='degree')
-@cache.cached(timeout=3600)  # Cache the result for 1 hour
+@cache.cached(timeout=3600) # Cache the result for 1 hour
 def degree():
     degreeDF = cache.get('degreeDF')
     if degreeDF is None:
@@ -108,9 +124,8 @@ def degree():
     table_rows = degreeDF.values.tolist()
     return render_template('centrality_degree.html', table_headers=table_headers, table_rows=table_rows)
 
-
 @app.route('/eigenvector', endpoint='eigenvector')
-@cache.cached(timeout=3600)  # Cache the result for 1 hour
+@cache.cached(timeout=3600) # Cache the result for 1 hour
 def eigenvector():
     eigenvectorDF = cache.get('eigenvectorDF')
     if eigenvectorDF is None:
@@ -120,9 +135,8 @@ def eigenvector():
     table_rows = eigenvectorDF.values.tolist()
     return render_template('centrality_eigenvector.html', table_headers=table_headers, table_rows=table_rows)
 
-
 @app.route('/closeness', endpoint='closeness')
-@cache.cached(timeout=3600)  # Cache the result for 1 hour
+@cache.cached(timeout=3600) # Cache the result for 1 hour
 def closeness():
     closenessDF = cache.get('closenessDF')
     if closenessDF is None:
@@ -132,9 +146,8 @@ def closeness():
     table_rows = closenessDF.values.tolist()
     return render_template('centrality_closeness.html', table_headers=table_headers, table_rows=table_rows)
 
-
 @app.route('/betwenness', endpoint='betwenness')
-@cache.cached(timeout=3600)  # Cache the result for 1 hour
+@cache.cached(timeout=3600) # Cache the result for 1 hour
 def betwenness():
     betwennessDF = cache.get('betwennessDF')
     if betwennessDF is None:
@@ -144,9 +157,8 @@ def betwenness():
     table_rows = betwennessDF.values.tolist()
     return render_template('centrality_betwenness.html', table_headers=table_headers, table_rows=table_rows)
 
-
 @app.route('/load', endpoint='load')
-@cache.cached(timeout=3600)  # Cache the result for 1 hour
+@cache.cached(timeout=3600) # Cache the result for 1 hour
 def load():
     loadDF = cache.get('loadDF')
     if loadDF is None:
@@ -155,7 +167,6 @@ def load():
     table_headers = list(loadDF.columns.values)
     table_rows = loadDF.values.tolist()
     return render_template('centrality_load.html', table_headers=table_headers, table_rows=table_rows)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
