16904bb2ad63214cd255ff930b2d73b7be8374fa
Tejas Patil <tejas.coder@gmail.com>
2023-03-10 09:09:27 +0000

    updated code
    Used Jqeuy tables and, used cache so everytime home and centrality wont calculate matrics again

--- application/app.py
+++ application/app.py
@@ -2,9 +2,12 @@ from flask import Flask, request, render_template, redirect, url_for, session
 import csv
 from src.NetworkGraphs import *
 import scipy as sp
+from flask_caching import Cache
 
 app = Flask(__name__)
 app.secret_key = 'my-secret-key' # set a secret key for the session
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache.init_app(app)
 
 filepath = './datasets/Railway.csv'
 
@@ -39,9 +42,17 @@ def upload():
     return redirect(url_for('home'))
 
 @app.route('/home')
+@cache.cached(timeout=3600) # Cache the result for 1 hour
 def home():
     # Get the filename from the session variable
     filename = session['filename']
+    
+    global_metrics = cache.get('global_metrics')
+    if global_metrics is None:
+        global_metrics = compute_global_metrics(networkGraphs)
+        cache.set('global_metrics', global_metrics)
+    table_headers = list(global_metrics.columns.values)
+    table_rows = global_metrics.values.tolist()
 
     # Open the CSV file and read its contents
     with open('uploads/' + filename, 'r') as file:
@@ -49,17 +60,13 @@ def home():
         header = next(reader)  # skip the header row
         data = [header]  # initialize the data list with the header row
         for i, row in enumerate(reader):
-            if i < 50:
+            if i < 100:
                 data.append(row)
             else:
                 break
 
-    global_metrics = compute_global_metrics(networkGraphs)
-
     # Pass the data to the HTML template
-    return render_template('home.html', data=data,  metrics_table=
-                           global_metrics.to_html(classes='table', table_id="datatablesSimple", header="true"),
-                           datasetDf=df[:100].to_html(classes='table', table_id="datasetSimple", header="true"))
+    return render_template('home.html', data=data, table_headers=table_headers, table_rows=table_rows)
 
 @app.route('/visualise/static', endpoint='my_static')
 def static():
@@ -71,10 +78,16 @@ def dynamic():
     return render_template('dynamic.html')
 
 @app.route('/centrality', endpoint='centrality')
-def dynamic():
-    allCentralityDF = compute_node_metrics(networkGraphs, directed=False)
-    return render_template('centrality.html',  datasetDf=allCentralityDF.to_html(classes='table', table_id="datatablesSimple",
-                                                             header="true"))
+@cache.cached(timeout=3600) # Cache the result for 1 hour
+def centrality():
+    allCentralityDF = cache.get('allCentralityDF')
+    if allCentralityDF is None:
+        allCentralityDF = compute_node_metrics(networkGraphs, directed=False)
+        cache.set('allCentralityDF', allCentralityDF)
+    table_headers = list(allCentralityDF.columns.values)
+    table_rows = allCentralityDF.values.tolist()
+    return render_template('centrality.html', table_headers=table_headers, table_rows=table_rows)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
