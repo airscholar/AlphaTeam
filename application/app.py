f230428b6a7fb82dda6e195d3e002f9278f4331c
Tejas Patil <tejas.coder@gmail.com>
2023-03-20 17:16:14 +0000

    update
    1) All clusters added (complted sprint 3)
    2) moved general metrics to seperate page

--- application/app.py
+++ application/app.py
@@ -141,8 +141,11 @@ def home():
         cache.set('global_metrics', global_metrics)
     
     # Pass the data to the HTML template
-    return render_template('home.html', data=networkGraphs.df.head(100), example=global_metrics)
+    return render_template('home.html', example=global_metrics)
 
+@app.route('/uploaded_dataset')
+def uploaded_dataset():
+    return render_template('display_base_dataset.html', data=networkGraphs.df.head(100))
 #-------------------------------------------VISUALISATION-----------------------------------
 
 @app.route('/visualise/static', methods=['GET', 'POST'], endpoint='my_static')
@@ -1394,13 +1397,214 @@ def clustering_girvan_newman():
     return render_template('clustering_girvan_newman.html', example=df,
     multi_toggle=multi_toggle, dynamic_toggle=dynamic_toggle, directed_toggle=directed_toggle, layout=layout, graph1=graph_path1, method_name='Girvan Newman')
 
+@app.route('/clustering/edge_betweenness', endpoint='clustering_edge_betweenness', methods=['GET', 'POST'])
+def clustering_edge_betweenness():
+    filename2 = session['filename2']
+    clusterType = 'edge_betweenness'
+    multi_toggle = True
+    dynamic_toggle = False
+    directed_toggle = False
+    layout = 'map'
+    
+    if request.method == 'POST':
+            multi_toggle = bool(request.form.get('multi_toggle'))
+            dynamic_toggle = bool(request.form.get('dynamic_toggle'))
+            directed_toggle = bool(request.form.get('directed_toggle'))
+            layout = request.form.get('layout')
+            df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+            session['graph_name1'] = graph_name1
+    else:
+        df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+        session['graph_name1'] = graph_name1
+    graph1 = session['graph_name1']
+ 
+    if graph1 == 'no_graph.html':
+        graph_path1 = '../static/' + graph1
+    else:
+        graph_path1 = '../static/uploads/' + filename2 + '/' + graph1
+
+    return render_template('clustering_edge_betweenness.html', example=df,
+    multi_toggle=multi_toggle, dynamic_toggle=dynamic_toggle, directed_toggle=directed_toggle, layout=layout, graph1=graph_path1, method_name='Edge Betweenness')
+
+@app.route('/clustering/k_clique', endpoint='clustering_k_clique', methods=['GET', 'POST'])
+def clustering_k_clique():
+    filename2 = session['filename2']
+    clusterType = 'k_clique'
+    multi_toggle = True
+    dynamic_toggle = False
+    directed_toggle = False
+    layout = 'map'
+    
+    if request.method == 'POST':
+            multi_toggle = bool(request.form.get('multi_toggle'))
+            dynamic_toggle = bool(request.form.get('dynamic_toggle'))
+            directed_toggle = bool(request.form.get('directed_toggle'))
+            layout = request.form.get('layout')
+            df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+            session['graph_name1'] = graph_name1
+    else:
+        df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+        session['graph_name1'] = graph_name1
+    graph1 = session['graph_name1']
+ 
+    if graph1 == 'no_graph.html':
+        graph_path1 = '../static/' + graph1
+    else:
+        graph_path1 = '../static/uploads/' + filename2 + '/' + graph1
+
+    return render_template('clustering_k_clique.html', example=df,
+    multi_toggle=multi_toggle, dynamic_toggle=dynamic_toggle, directed_toggle=directed_toggle, layout=layout, graph1=graph_path1, method_name='K Clique')
+
+@app.route('/clustering/spectral', endpoint='clustering_spectral', methods=['GET', 'POST'])
+def clustering_spectral():
+    filename2 = session['filename2']
+    clusterType = 'spectral'
+    multi_toggle = True
+    dynamic_toggle = False
+    directed_toggle = False
+    layout = 'map'
+    
+    if request.method == 'POST':
+            multi_toggle = bool(request.form.get('multi_toggle'))
+            dynamic_toggle = bool(request.form.get('dynamic_toggle'))
+            directed_toggle = bool(request.form.get('directed_toggle'))
+            layout = request.form.get('layout')
+            df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+            session['graph_name1'] = graph_name1
+    else:
+        df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+        session['graph_name1'] = graph_name1
+    graph1 = session['graph_name1']
+ 
+    if graph1 == 'no_graph.html':
+        graph_path1 = '../static/' + graph1
+    else:
+        graph_path1 = '../static/uploads/' + filename2 + '/' + graph1
+
+    return render_template('clustering_spectral.html', example=df,
+    multi_toggle=multi_toggle, dynamic_toggle=dynamic_toggle, directed_toggle=directed_toggle, layout=layout, graph1=graph_path1, method_name='spectrals')
+
+@app.route('/clustering/kmeans', endpoint='clustering_kmeans', methods=['GET', 'POST'])
+def clustering_kmeans():
+    filename2 = session['filename2']
+    clusterType = 'kmeans'
+    multi_toggle = True
+    dynamic_toggle = False
+    directed_toggle = False
+    layout = 'map'
+    
+    if request.method == 'POST':
+            multi_toggle = bool(request.form.get('multi_toggle'))
+            dynamic_toggle = bool(request.form.get('dynamic_toggle'))
+            directed_toggle = bool(request.form.get('directed_toggle'))
+            layout = request.form.get('layout')
+            df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+            session['graph_name1'] = graph_name1
+    else:
+        df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+        session['graph_name1'] = graph_name1
+    graph1 = session['graph_name1']
+ 
+    if graph1 == 'no_graph.html':
+        graph_path1 = '../static/' + graph1
+    else:
+        graph_path1 = '../static/uploads/' + filename2 + '/' + graph1
+
+    return render_template('clustering_kmeans.html', example=df,
+    multi_toggle=multi_toggle, dynamic_toggle=dynamic_toggle, directed_toggle=directed_toggle, layout=layout, graph1=graph_path1, method_name='KMeans')
+
+@app.route('/clustering/agglomerative', endpoint='clustering_agglomerative', methods=['GET', 'POST'])
+def clustering_agglomerative():
+    filename2 = session['filename2']
+    clusterType = 'agglomerative'
+    multi_toggle = True
+    dynamic_toggle = False
+    directed_toggle = False
+    layout = 'map'
+    
+    if request.method == 'POST':
+            multi_toggle = bool(request.form.get('multi_toggle'))
+            dynamic_toggle = bool(request.form.get('dynamic_toggle'))
+            directed_toggle = bool(request.form.get('directed_toggle'))
+            layout = request.form.get('layout')
+            df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+            session['graph_name1'] = graph_name1
+    else:
+        df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+        session['graph_name1'] = graph_name1
+    graph1 = session['graph_name1']
+ 
+    if graph1 == 'no_graph.html':
+        graph_path1 = '../static/' + graph1
+    else:
+        graph_path1 = '../static/uploads/' + filename2 + '/' + graph1
+
+    return render_template('clustering_agglomerative.html', example=df,
+    multi_toggle=multi_toggle, dynamic_toggle=dynamic_toggle, directed_toggle=directed_toggle, layout=layout, graph1=graph_path1, method_name='Agglomerative')
+
+@app.route('/clustering/dbscan', endpoint='clustering_dbscan', methods=['GET', 'POST'])
+def clustering_dbscan():
+    filename2 = session['filename2']
+    clusterType = 'dbscan'
+    multi_toggle = True
+    dynamic_toggle = False
+    directed_toggle = False
+    layout = 'map'
+    
+    if request.method == 'POST':
+            multi_toggle = bool(request.form.get('multi_toggle'))
+            dynamic_toggle = bool(request.form.get('dynamic_toggle'))
+            directed_toggle = bool(request.form.get('directed_toggle'))
+            layout = request.form.get('layout')
+            df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+            session['graph_name1'] = graph_name1
+    else:
+        df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+        session['graph_name1'] = graph_name1
+    graph1 = session['graph_name1']
+ 
+    if graph1 == 'no_graph.html':
+        graph_path1 = '../static/' + graph1
+    else:
+        graph_path1 = '../static/uploads/' + filename2 + '/' + graph1
+
+    return render_template('clustering_dbscan.html', example=df,
+    multi_toggle=multi_toggle, dynamic_toggle=dynamic_toggle, directed_toggle=directed_toggle, layout=layout, graph1=graph_path1, method_name='Dbscan')
+
+@app.route('/clustering/hierarchical', endpoint='clustering_hierarchical', methods=['GET', 'POST'])
+def clustering_hierarchical():
+    filename2 = session['filename2']
+    clusterType = 'hierarchical'
+    multi_toggle = True
+    dynamic_toggle = False
+    directed_toggle = False
+    layout = 'map'
+    
+    if request.method == 'POST':
+            multi_toggle = bool(request.form.get('multi_toggle'))
+            dynamic_toggle = bool(request.form.get('dynamic_toggle'))
+            directed_toggle = bool(request.form.get('directed_toggle'))
+            layout = request.form.get('layout')
+            df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+            session['graph_name1'] = graph_name1
+    else:
+        df, graph_name1 = plot_cluster(networkGraphs, clusterType, dynamic=dynamic_toggle, layout=layout)
+        session['graph_name1'] = graph_name1
+    graph1 = session['graph_name1']
+ 
+    if graph1 == 'no_graph.html':
+        graph_path1 = '../static/' + graph1
+    else:
+        graph_path1 = '../static/uploads/' + filename2 + '/' + graph1
+
+    return render_template('clustering_hierarchical.html', example=df,
+    multi_toggle=multi_toggle, dynamic_toggle=dynamic_toggle, directed_toggle=directed_toggle, layout=layout, graph1=graph_path1, method_name='Hierarchical')
+
 #-------------------------------------------RESILIENCE_ANALYSIS-----------------------------------
 
 @app.route('/resilience/random', endpoint='resilience_random', methods=['GET', 'POST'])
 def resilience_analyisis_random():
     return render_template('resilience_analyisis_random.html')
-
-
 
 #-------------------------------------------MAIN--------------------------------------------
 if __name__ == '__main__':
