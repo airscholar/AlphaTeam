1860cdfa205cddd4abf07b28fbf21da048d5b7b3
sferez <simeon.ferez@gmail.com>
2023-03-28 13:25:57 +0100

    feat: resilience

--- src/resilience.py
+++ src/resilience.py
@@ -39,7 +39,38 @@ def resilience(networkGraph, attack, **kwargs):
     :return: NetworkGraph with the nodes removed
     :rtype: NetworkGraph
     """
-    return 0
+    if attack == "random":
+        for key in kwargs.keys():
+            if key not in ["number_of_nodes", "number_of_edges"]:
+                print(f"Argument {key} not recognized")
+                return 0
+        nrb_nodes = kwargs["number_of_nodes"] if "number_of_nodes" in kwargs.keys() else 0
+        nrb_edges = kwargs["number_of_edges"] if "number_of_edges" in kwargs.keys() else 0
+        return resilience_random(networkGraph, nrb_nodes, nrb_edges)
+
+    elif attack == "malicious":
+        for key in kwargs.keys():
+            if key not in ["metric", "number_of_nodes", "threshold"]:
+                print(f"Argument {key} not recognized")
+                return 0
+        metric = kwargs["metric"]
+        nrb_nodes = kwargs["number_of_nodes"] if "number_of_nodes" in kwargs.keys() else 0
+        threshold = kwargs["threshold"] if "threshold" in kwargs.keys() else 0
+        return resilience_malicious(networkGraph, metric, nrb_nodes, threshold)
+
+    elif attack == "cluster":
+        for key in kwargs.keys():
+            if key not in ["cluster_algorithm", "total_clusters", "number_of_clusters"]:
+                print(f"Argument {key} not recognized")
+                return 0
+        cluster_algorithm = kwargs["cluster_algorithm"]
+        total_clusters = kwargs["total_clusters"]
+        number_of_clusters = kwargs["number_of_clusters"]
+        return resilience_cluster(networkGraph, cluster_algorithm, total_clusters, number_of_clusters)
+
+    else:
+        print("Attack not recognized")
+        return 0
 
 
 # ------------------------------------------------------------------------------------------
