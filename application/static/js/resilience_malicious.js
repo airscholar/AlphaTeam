29be76253b3cf0f3eb78a0e14062b6745d00dc96
Tejas Patil <tejas.coder@gmail.com>
2023-04-03 18:30:45 +0100

    update
    testing of cluster 1

--- application/static/js/resilience_malicious.js
+++ application/static/js/resilience_malicious.js
@@ -63,3 +63,35 @@ function performResilienceMetrics(data, plot_type, section) {
     });
 }
 
+function performResilienceCluster(data, plot_type, section) {
+    $.ajax({
+        url: BASE_URL + data.session_id + '/' + data.type_of_attack + '/' + '&layout='
+            + data.layout + '&noOfClusters=' + data.number_of_clusters,
+        type: 'GET',
+        mode: 'no-cors',
+        success: function (data) {
+            let beforeFrame, afterFrame;
+            const prefix = section + '_' + plot_type;
+
+            if (plot_type === 'louvain' || plot_type === 'greedy_modularity' || plot_type === 'label_propagation' || plot_type === 'asyn_lpa'
+                || plot_type === 'k_clique' || plot_type === 'spectral' || plot_type === 'kmeans'
+                || plot_type === 'agglomerative' || plot_type === 'dbscan') {
+                beforeFrame = document.getElementById(prefix + '_before');
+                afterFrame = document.getElementById(prefix + '_after');
+            }
+
+            beforeLayoutPath = data.network_before.replace('application/', '');
+            afterLayoutPath = data.network_after.replace('application/', '');
+
+            $(beforeFrame).attr("src", beforeLayoutPath);
+            $(afterFrame).attr("src", afterLayoutPath);
+
+
+        },
+        error: function (data) {
+            alert('An error occurred. Please try again.');
+            console.log('ERROR', data);
+        }
+    });
+}
+
