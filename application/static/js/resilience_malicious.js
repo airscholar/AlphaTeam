233770a41a03ccdb1f6f8c67ccdd84d7208481d3
Yusuf Ganiyu <airscholar@gmail.com>
2023-04-03 19:27:52 +0100

    feat: added dynamic datatable for global metrics

--- application/static/js/resilience_malicious.js
+++ application/static/js/resilience_malicious.js
@@ -10,11 +10,11 @@ function performResilienceMalicious(data) {
         type: 'GET',
         mode: 'no-cors',
         success: function (data) {
-            var beforeFrame = document.getElementById('before_frame');
-            var afterFrame = document.getElementById('after_frame');
+            let beforeFrame = document.getElementById('before_frame');
+            let afterFrame = document.getElementById('after_frame');
 
-            beforePath = data.network_before.replace('application/', '');
-            afterPath = data.network_after.replace('application/', '');
+            const beforePath = data.network_before.replace('application/', '');
+            const afterPath = data.network_after.replace('application/', '');
 
             $(beforeFrame).attr("src", beforePath);
             $(afterFrame).attr("src", afterPath);
@@ -42,8 +42,8 @@ function performResilienceMetrics(data, plot_type, section) {
                 afterFrame = document.getElementById(prefix + '_after');
             }
 
-            beforeLayoutPath = data.network_before.replace('application/', '');
-            afterLayoutPath = data.network_after.replace('application/', '');
+            const beforeLayoutPath = data.network_before.replace('application/', '');
+            const afterLayoutPath = data.network_after.replace('application/', '');
 
             $(beforeFrame).attr("src", beforeLayoutPath);
             $(afterFrame).attr("src", afterLayoutPath);
@@ -55,4 +55,49 @@ function performResilienceMetrics(data, plot_type, section) {
             console.log('ERROR', data);
         }
     });
+}
+
+function retrieveGeneralMetrics(data) {
+    $.ajax({
+        url: `${BASE_URL}${data.session_id}/${data.type_of_attack}/global_metrics`,
+        type: 'GET',
+        data: {
+            multi_toggle: data.multi_toggle,
+            directed_toggle: data.directed_toggle,
+            layout: data.layout,
+            number_of_clusters: data.number_of_clusters,
+            attack_type: data.type_of_attack,
+            number_of_nodes_malicious: data.number_of_nodes_malicious,
+        },
+        success: function (data) {
+            const beforeTable = document.getElementById('GM_Table_before');
+            const afterTable = document.getElementById('GM_Table_after');
+            const resBefore = JSON.parse(data.data_before);
+            const resAfter = JSON.parse(data.data_after);
+            const columns = resBefore.columns;
+
+            const createTable = (tableElem, data) => {
+                const headerRow = tableElem.createTHead().insertRow(0);
+                for (let i = 0; i < columns.length; i++) {
+                    headerRow.insertCell(i).innerHTML = columns[i];
+                }
+                const body = tableElem.createTBody();
+                for (let i = 0; i < data.length; i++) {
+                    const row = body.insertRow(i);
+                    for (let j = 0; j < data[i].length; j++) {
+                        row.insertCell(j).innerHTML = data[i][j];
+                    }
+                }
+                $(tableElem).DataTable();
+            };
+
+            createTable(beforeTable, resBefore.data);
+            createTable(afterTable, resAfter.data);
+        },
+        error: function (data) {
+            console.log('ERROR', data);
+        }
+    });
+
+
 }
