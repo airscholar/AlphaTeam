32a7a570fbc832da8f3ee41a7f94fa214b82ecef
sferez <simeon.ferez@gmail.com>
2023-04-03 17:41:41 +0100

    fix: paths, toggle buttons

--- backend/resilience/resilience.py
+++ backend/resilience/resilience.py
@@ -4,57 +4,20 @@ from flask_jsonpify import jsonify
 from src.resilience import resilience
 from src.utils import get_networkGraph, set_networkGraph
 from src.visualisation import *
+from backend.common.common import *
 
 resilience_bp = Blueprint('resilience', __name__, url_prefix="/api/v1/resilience")
 
 
-def extract_args():
-    args = request.args
-
-    attack_type = args.get('attack_type')
-    number_of_nodes_malicious = args.get('number_of_nodes_malicious')
-    number_of_threshold = args.get('number_of_threshold')
-    operator = args.get('operator')
-    directed_toggle = args.get('directed_toggle')
-    multi_toggle = args.get('multi_toggle')
-    layout = args.get('layout')
-
-    return attack_type, number_of_nodes_malicious, number_of_threshold, operator, directed_toggle, multi_toggle, layout
-
-
-@resilience_bp.route('<session_id>/malicious')
-def compute_malicious(session_id):
-    attack_type, number_of_nodes_malicious, number_of_threshold, \
-        operator, directed_toggle, multi_toggle, layout = extract_args()
-
-    networkGraphs = get_networkGraph(session_id)
-
-    networkGraphs2, df = resilience(networkGraphs, attack='malicious', metric=attack_type,
-                                    number_of_nodes=number_of_nodes_malicious, threshold=number_of_threshold,
-                                    operator=operator)
-
-    session_id2 = session_id + '_resilience'
-    set_networkGraph(networkGraphs2, session_id2)
-
-    before = plot_network(networkGraphs, layout=layout, dynamic=False, fullPath=True)
-    after = plot_network(networkGraphs2, layout=layout, dynamic=False, fullPath=True)
-    heatmap_before = plot_heatmap(networkGraphs, fullPath=True)
-    heatmap_after = plot_heatmap(networkGraphs2, fullPath=True)
-
-    df_json = df.to_json(orient='split')
-
-    return jsonify({"message": "Success", "data": df_json, "network_before": before, "network_after": after,
-                    "heatmap_before": heatmap_before, "heatmap_after": heatmap_after})
-
-
 @resilience_bp.route('<session_id>/<metric>/<plot_type>/')
 def compute_metrics(session_id, metric, plot_type):
-    attack_type, number_of_nodes_malicious, number_of_threshold, \
-        operator, directed_toggle, multi_toggle, layout = extract_args()
+    directed_toggle = get_directed_toggle(request.args)
+    multi_toggle = get_multi_toggle(request.args)
+    layout = get_layout(request.args)
 
     networkGraphs = get_networkGraph(session_id)
-
     networkGraphs2 = get_networkGraph(session_id + '_resilience')
+
     df = None
     df1 = None
     file_name = None
