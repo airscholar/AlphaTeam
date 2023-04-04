35220463c1695de2e5e09cc5c30b2383d204f906
Tejas Patil <tejas.coder@gmail.com>
2023-04-04 14:00:50 +0100

    update
    chnages before pull 2pm

--- application/routes/resilience_routes.py
+++ application/routes/resilience_routes.py
@@ -70,7 +70,6 @@ def resilience_analyisis_random():
         number_of_edges = request.form.get('number_of_edges')
         number_of_nodes_random = request.form.get('number_of_nodes_random')
         number_of_clusters = request.form.get('number_of_clusters')
-        operator = res_operator[request.form.get('threshold_operator')]
 
         # visualise before and after
         json_data = requests.get(BASE_URL + f'/resilience/{filename2}/random',
@@ -92,27 +91,26 @@ def resilience_analyisis_random():
     graph_path1 = '../static/no_graph.html'
     graph_path2 = '../static/no_graph.html'
 
-    number_of_nodes_random = None
-    number_of_edges = None
+    cluster_algorithm = None
+    total_clusters = None
     number_of_clusters = None
 
     if request.method == 'POST':
-        number_of_edges = request.form.get('number_of_edges')
-        number_of_nodes_random = request.form.get('number_of_nodes_random')
-        number_of_clusters = request.form.get('number_of_clusters')
-        operator = res_operator[request.form.get('threshold_operator')]
+        cluster_algorithm = request.form.get('layout3')
+        total_clusters = request.form.get('number_of_cluster_to_generate')
+        number_of_clusters = request.form.get('cluster_to_attack')
 
         # visualise before and after
         json_data = requests.get(BASE_URL + f'/resilience/{filename2}/random',
-                                 params={'number_of_nodes_random': number_of_nodes_random,
-                                         'number_of_clusters':number_of_clusters,
-                                         'number_of_edges': number_of_edges}).json()
+                                 params={'cluster_algorithm': cluster_algorithm,
+                                         'total_clusters':total_clusters,
+                                         'number_of_clusters': number_of_clusters}).json()
 
         graph_path1 = json_data['network_before'].replace('application/', '')
         graph_path2 = json_data['network_after'].replace('application/', '')
 
     return render_template('resilience/resilience_analyisis_cluster.html', session_id=filename2,
-                           number_of_edges=number_of_edges, number_of_clusters=number_of_clusters,
-                           number_of_nodes_random=number_of_nodes_random, graph_path1=graph_path1,
+                           layout3=cluster_algorithm, cluster_to_attack=number_of_clusters,
+                           number_of_cluster_to_generate=total_clusters, graph_path1=graph_path1,
                            graph_path2=graph_path2)
   
--- application/static/js/resilience_cluster.js
+++ application/static/js/resilience_cluster.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'http://localhost:8000/api/v1/resilience/';
 
-function performResilienceRandom(data) {
+function performResilienceClusterForm(data) {
     console.log(data)
     $.ajax({
-        url: `${BASE_URL}${data.session_id}/random?&number_of_edges=${data.number_of_edges}&number_of_nodes=${data.number_of_nodes_random}`,
+        url: `${BASE_URL}${data.session_id}/cluster?&cluster_algorithm=${data.cluster_algorithm}&total_clusters=${data.total_clusters}&number_of_clusters=${data.number_of_clusters}`,
         type: 'GET',
         mode: 'no-cors',
         success: function (data) {
@@ -30,7 +30,7 @@ function performResilienceRandom(data) {
 
 function performResilienceMetrics(data, plot_type, section) {
     $.ajax({
-        url: `${BASE_URL}${data.session_id}/random?multi_toggle=${data.multi_toggle}&directed_toggle=${data.directed_toggle}&layout=${data.layout}&number_of_clusters=${data.number_of_clusters}&number_of_edges=${data.number_of_edges}&attack_type=${data.type_of_attack}&number_of_nodes=${data.number_of_nodes_random}`,
+        url: `${BASE_URL}${data.session_id}/cluster?multi_toggle=${data.multi_toggle}&directed_toggle=${data.directed_toggle}&layout=${data.layout}&cluster_algorithm=${data.cluster_algorithm}&total_clusters=${data.total_clusters}&number_of_clusters=${data.number_of_clusters}&attack_type=${data.type_of_attack}`,
         type: 'GET',
         mode: 'no-cors',
         success: function (data) {
@@ -59,7 +59,7 @@ function performResilienceMetrics(data, plot_type, section) {
 
 function performResilienceCluster(data, plot_type, section) {
     $.ajax({
-        url: `${BASE_URL}${data.session_id}/${plot_type}?layout=${data.layout}&noOfClusters=${data.number_of_clusters}`,
+        url: `${BASE_URL}${data.session_id}/${data.cluster_algorithm}?layout=${data.layout}&noOfClusters=${data.number_of_clusters}`,
         type: 'GET',
         mode: 'no-cors',
         success: function (data) {
