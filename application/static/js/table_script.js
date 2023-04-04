2fb9a5ebc1a58faddb64fa5ff873dcbbb9b1a579
sferez <simeon.ferez@gmail.com>
2023-04-04 18:35:41 +0100

    fix: table JS cleaning

--- application/static/js/table_script.js
+++ application/static/js/table_script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('table').each(function () {
         // disablle cleaning if the table is the global metrics table
         if (this.id === 'global-metrics') {
+            console.log('global metrics table')
             $(this).DataTable({
                 dom: 'Bfrtip',
                 buttons: [
@@ -14,6 +15,16 @@ $(document).ready(function () {
                 "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]], // Customize page length menu
                 "columnDefs": [{
                     "targets": "_all",
+                    "render": function (data, type, row) {
+                        if (typeof data === 'string') {
+                            if (data.length > 6) {
+                                if (data.startsWith('0') || data.startsWith('1') || data.startsWith('2') || data.startsWith('3') || data.startsWith('4') || data.startsWith('5') || data.startsWith('6') || data.startsWith('7') || data.startsWith('8') || data.startsWith('9')) {
+                                    return data.substring(0, 7); // Limit the length to 6 if the string doesn't start with '0x'
+                                }
+                            }
+                        }
+                        return data;
+                    }
                 }]
             });
         } else {
