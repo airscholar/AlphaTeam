20179feec12118810bd1642db80659eaa32e3e45
Tejas Patil <tejas.coder@gmail.com>
2023-04-05 17:30:28 +0100

    update
    resilience template update

--- application/routes/resilience_routes.py
+++ application/routes/resilience_routes.py
@@ -72,3 +72,33 @@ def resilience_analyisis_random():
                            layout3=cluster_algorithm, cluster_to_attack=number_of_clusters,
                            number_of_cluster_to_generate=total_clusters, graph_path1=graph_path1,
                            graph_path2=graph_path2)
+
+@resilience_routes.route('/resilience/custom', endpoint='resilience_custom', methods=['GET', 'POST'])
+def resilience_analyisis_custom():
+    filename2 = session['filename2']
+    graph_path1 = '../static/no_graph.html'
+    graph_path2 = '../static/no_graph.html'
+
+    cluster_algorithm = None
+    total_clusters = None
+    number_of_clusters = None
+
+    if request.method == 'POST':
+        cluster_algorithm = request.form.get('layout3')
+        total_clusters = request.form.get('number_of_cluster_to_generate')
+        number_of_clusters = request.form.get('cluster_to_attack')
+
+        # visualise before and after
+        json_data = requests.get(BASE_URL + f'/resilience/{filename2}/random',
+                                 params={'cluster_algorithm': cluster_algorithm,
+                                         'total_clusters': total_clusters,
+                                         'number_of_clusters': number_of_clusters}).json()
+
+        graph_path1 = json_data['network_before'].replace('application/', '')
+        graph_path2 = json_data['network_after'].replace('application/', '')
+
+    return render_template('resilience/resilience_analyisis_custom.html', session_id=filename2,
+                           layout3=cluster_algorithm, cluster_to_attack=number_of_clusters,
+                           number_of_cluster_to_generate=total_clusters, graph_path1=graph_path1,
+                           graph_path2=graph_path2)
+
