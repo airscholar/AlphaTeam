f69957b6803dad73866749d86c195234a519e56b
Tejas Patil <tejas.coder@gmail.com>
2023-04-06 21:46:48 +0100

    Update resilience_routes.py

--- application/routes/resilience_routes.py
+++ application/routes/resilience_routes.py
@@ -45,7 +45,7 @@ def resilience_analyisis_random():
 
 
 @resilience_routes.route('/resilience/cluster', endpoint='resilience_cluster', methods=['GET', 'POST'])
-def resilience_analyisis_random():
+def resilience_analyisis_cluster():
     filename2 = session['filename2']
     graph_path1 = '../static/no_graph.html'
     graph_path2 = '../static/no_graph.html'
@@ -53,20 +53,6 @@ def resilience_analyisis_random():
     cluster_algorithm = None
     total_clusters = None
     number_of_clusters = None
-
-    if request.method == 'POST':
-        cluster_algorithm = request.form.get('layout3')
-        total_clusters = request.form.get('number_of_cluster_to_generate')
-        number_of_clusters = request.form.get('cluster_to_attack')
-
-        # visualise before and after
-        json_data = requests.get(BASE_URL + f'/resilience/{filename2}/random',
-                                 params={'cluster_algorithm': cluster_algorithm,
-                                         'total_clusters': total_clusters,
-                                         'number_of_clusters': number_of_clusters}).json()
-
-        graph_path1 = json_data['network_before'].replace('application/', '')
-        graph_path2 = json_data['network_after'].replace('application/', '')
 
     return render_template('resilience/resilience_analyisis_cluster.html', session_id=filename2,
                            layout3=cluster_algorithm, cluster_to_attack=number_of_clusters,
@@ -83,19 +69,19 @@ def resilience_analyisis_custom():
     total_clusters = None
     number_of_clusters = None
 
-    if request.method == 'POST':
-        cluster_algorithm = request.form.get('layout3')
-        total_clusters = request.form.get('number_of_cluster_to_generate')
-        number_of_clusters = request.form.get('cluster_to_attack')
+    # if request.method == 'POST':
+    #     cluster_algorithm = request.form.get('layout3')
+    #     total_clusters = request.form.get('number_of_cluster_to_generate')
+    #     number_of_clusters = request.form.get('cluster_to_attack')
 
-        # visualise before and after
-        json_data = requests.get(BASE_URL + f'/resilience/{filename2}/random',
-                                 params={'cluster_algorithm': cluster_algorithm,
-                                         'total_clusters': total_clusters,
-                                         'number_of_clusters': number_of_clusters}).json()
+    #     # visualise before and after
+    #     json_data = requests.get(BASE_URL + f'/resilience/{filename2}/random',
+    #                              params={'cluster_algorithm': cluster_algorithm,
+    #                                      'total_clusters': total_clusters,
+    #                                      'number_of_clusters': number_of_clusters}).json()
 
-        graph_path1 = json_data['network_before'].replace('application/', '')
-        graph_path2 = json_data['network_after'].replace('application/', '')
+    #     graph_path1 = json_data['network_before'].replace('application/', '')
+    #     graph_path2 = json_data['network_after'].replace('application/', '')
 
     return render_template('resilience/resilience_analyisis_custom.html', session_id=filename2,
                            layout3=cluster_algorithm, cluster_to_attack=number_of_clusters,
