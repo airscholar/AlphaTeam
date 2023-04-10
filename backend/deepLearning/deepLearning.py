8f7245e78bb9ba2920cf9b197ba276c4015a6dc0
sferez <simeon.ferez@gmail.com>
2023-04-10 12:30:52 +0100

    fix: rm ../application from filename

--- backend/deepLearning/deepLearning.py
+++ backend/deepLearning/deepLearning.py
@@ -41,7 +41,6 @@ def node_embedding(session_id):
 
     df_json = df.to_json(orient='split')
     filename = filename.replace('../application/', '')
-    print(filename)
     return jsonify({'message': 'Success', 'data': df_json, 'filename': filename})
 
 
@@ -71,6 +70,5 @@ def embedding_clustering(session_id):
         raise ValueError('Layout not supported, please choose between TSNE, PCA and UMAP, map, sfdp, twopi')
     df_json = df.to_json(orient='split')
     filename = filename.replace('../application/', '')
-    print(filename)
     return jsonify({'message': 'Success', 'data': df_json, 'filename': filename})
 
