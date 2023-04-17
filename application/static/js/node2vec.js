c24273013c435e0885c1082264105d631eae49d0
sferez <simeon.ferez@gmail.com>
2023-04-17 21:34:56 +0100

    feat: utils.js for createTable

--- application/static/js/node2vec.js
+++ application/static/js/node2vec.js
@@ -60,36 +60,3 @@ const performEmbClusteringVisualisation = (data) => {
         }
     });
 }
-
-const createTable = (tableElem, data, columns, isCluster = false) => {
-    tableElem.innerHTML = '';
-    tableElem.innerHTML = '';
-
-    if ($.fn.DataTable.isDataTable(tableElem)) {
-        let table = $(tableElem).DataTable();
-        if (table && table.table().node().parentNode) {
-            table.destroy();
-        }
-    }
-
-    if (tableElem.tHead) {
-        tableElem.tHead.remove();
-    }
-
-    const headerRow = tableElem.createTHead().insertRow(0);
-    for (let i = 0; i < columns.length; i++) {
-        headerRow.insertCell(i).innerHTML = columns[i];
-    }
-    const body = tableElem.createTBody();
-    for (let i = 0; i < data.length; i++) {
-        const row = body.insertRow(i);
-        for (let j = 0; j < data[i].length; j++) {
-            row.insertCell(j).innerHTML = data[i][j];
-        }
-    }
-    if (!isCluster) $(tableElem).DataTable();
-    else
-        $(tableElem).DataTable({
-            "scrollX": true,
-        });
-}
--- application/static/js/resilience.js
+++ application/static/js/resilience.js
@@ -1,34 +1,3 @@
-const createTable = (tableElem, data, columns, isCluster = false) => {
-    tableElem.innerHTML = '';
-    tableElem.innerHTML = '';
-
-    if ($.fn.DataTable.isDataTable(tableElem)) {
-        let table = $(tableElem).DataTable();
-        if (table && table.table().node().parentNode) {
-            table.destroy();
-        }
-    }
-
-    if (tableElem.tHead) {
-        tableElem.tHead.remove();
-    }
-
-    const headerRow = tableElem.createTHead().insertRow(0);
-    for (let i = 0; i < columns.length; i++) {
-        headerRow.insertCell(i).innerHTML = columns[i];
-    }
-    const body = tableElem.createTBody();
-    for (let i = 0; i < data.length; i++) {
-        const row = body.insertRow(i);
-        for (let j = 0; j < data[i].length; j++) {
-            row.insertCell(j).innerHTML = data[i][j];
-        }
-    }
-    $(tableElem).DataTable({
-        "scrollX": true,
-    });
-}
-
 const retrieveGeneralMetrics = (data) => {
     $.ajax({
         url: `${BASE_URL}${data.session_id}/global_metrics`,
