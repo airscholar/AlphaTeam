faebdc7616f5a319b92a0542e9fdd9fdc62cc4b4
Tejas Patil <65388565+tejas2292@users.noreply.github.com>
2023-04-24 23:06:18 +0100

    tooltips

--- application/routes/deepLearning/cluster_embedding_routes.py
+++ application/routes/deepLearning/cluster_embedding_routes.py
@@ -17,14 +17,18 @@ def clustering_embedding_kmeans():
     filename2 = session['filename2']
     clustering_alg = 'kmeans'
 
-    return render_template('deepLearning/node2vec/cluster/kmeans.html', session_id=filename2, clustering_alg=clustering_alg)
+    return render_template('deepLearning/node2vec/cluster/kmeans.html', session_id=filename2, clustering_alg=clustering_alg,
+    description=description['node2vec_kmeans'], tooltip_parameters=tooltips['parameters'], 
+    tooltip_layout=tooltips['layout_dropdown'], tooltip_number_of_clusters=tooltips['number_of_clusters'])
 
 @cluster_embedding_routes.route('/node2vec/clustering/embedding/spectral', endpoint='clustering_embedding_spectral',  methods=['GET', 'POST'])
 def clustering_embedding_spectral():
     filename2 = session['filename2']
     clustering_alg = 'spectral'
 
-    return render_template('deepLearning/node2vec/cluster/spectral.html', session_id=filename2, clustering_alg=clustering_alg)
+    return render_template('deepLearning/node2vec/cluster/spectral.html', session_id=filename2, clustering_alg=clustering_alg,
+    description=description['node2vec_spectral'], tooltip_parameters=tooltips['parameters'], 
+    tooltip_layout=tooltips['layout_dropdown'], tooltip_number_of_clusters=tooltips['number_of_clusters'])
 
 
 @cluster_embedding_routes.route('/node2vec/clustering/embedding/agglomerative', endpoint='clustering_embedding_agglomerative',  methods=['GET', 'POST'])
@@ -32,7 +36,9 @@ def clustering_embedding_agglomerative():
     filename2 = session['filename2']
     clustering_alg = 'agglomerative'
 
-    return render_template('deepLearning/node2vec/cluster/agglomerative.html', session_id=filename2, clustering_alg=clustering_alg)
+    return render_template('deepLearning/node2vec/cluster/agglomerative.html', session_id=filename2, clustering_alg=clustering_alg,
+    description=description['node2vec_agglomerative'], tooltip_parameters=tooltips['parameters'], 
+    tooltip_layout=tooltips['layout_dropdown'], tooltip_number_of_clusters=tooltips['number_of_clusters'])
 
 # Dl embedding
 
@@ -41,14 +47,20 @@ def clustering_embedding_kmeans():
     filename2 = session['filename2']
     clustering_alg = 'kmeans'
 
-    return render_template('deepLearning/dlembedding/cluster/kmeans.html', session_id=filename2, clustering_alg=clustering_alg)
+    return render_template('deepLearning/dlembedding/cluster/kmeans.html', session_id=filename2, clustering_alg=clustering_alg,
+    description=description['dlembedding_kmeans'], tooltip_dimension=tooltips['dimension'], 
+    tooltip_model_dropdown=tooltips['model_dropdown'], tooltip_layout=tooltips['layout_dropdown'], 
+    tooltip_features=tooltips['features_checkbox'], tooltip_number_of_clusters=tooltips['number_of_clusters'])
 
 @cluster_embedding_routes.route('/dlembedding/clustering/embedding/spectral', endpoint='dlembedding_clustering_embedding_spectral',  methods=['GET', 'POST'])
 def clustering_embedding_spectral():
     filename2 = session['filename2']
     clustering_alg = 'spectral'
 
-    return render_template('deepLearning/dlembedding/cluster/spectral.html', session_id=filename2, clustering_alg=clustering_alg)
+    return render_template('deepLearning/dlembedding/cluster/spectral.html', session_id=filename2, clustering_alg=clustering_alg,
+    description=description['dlembedding_spectral'], tooltip_dimension=tooltips['dimension'], 
+    tooltip_model_dropdown=tooltips['model_dropdown'], tooltip_layout=tooltips['layout_dropdown'], 
+    tooltip_features=tooltips['features_checkbox'], tooltip_number_of_clusters=tooltips['number_of_clusters'])
 
 
 @cluster_embedding_routes.route('/dlembedding/clustering/embedding/agglomerative', endpoint='dlembedding_clustering_embedding_agglomerative',  methods=['GET', 'POST'])
@@ -56,6 +68,9 @@ def clustering_embedding_agglomerative():
     filename2 = session['filename2']
     clustering_alg = 'agglomerative'
 
-    return render_template('deepLearning/dlembedding/cluster/agglomerative.html', session_id=filename2, clustering_alg=clustering_alg)
+    return render_template('deepLearning/dlembedding/cluster/agglomerative.html', session_id=filename2, clustering_alg=clustering_alg,
+    description=description['dlembedding_agglomerative'], tooltip_dimension=tooltips['dimension'], 
+    tooltip_model_dropdown=tooltips['model_dropdown'], tooltip_layout=tooltips['layout_dropdown'], 
+    tooltip_features=tooltips['features_checkbox'], tooltip_number_of_clusters=tooltips['number_of_clusters'])
 
 
--- application/routes/deepLearning/embedding_routes.py
+++ application/routes/deepLearning/embedding_routes.py
@@ -18,10 +18,16 @@ BASE_URL = 'http://localhost:8000/api/v1/deeplearning/'
 def embedding_visualisation():
     filename2 = session['filename2']
 
-    return render_template('deepLearning/node2vec/embedding.html', session_id=filename2)
+    return render_template('deepLearning/node2vec/embedding.html', session_id=filename2,
+    
+    description=description['node2vec_embedding'], tooltip_parameters=tooltips['parameters'], 
+    tooltip_layout=tooltips['layout_dropdown'])
 
 @embedding_routes.route('/dlembedding/embedding', endpoint='dlembedding_embedding', methods=['GET', 'POST'])
 def dlembedding_embedding_visualisation():
     filename2 = session['filename2']
 
-    return render_template('deepLearning/dlembedding/embedding.html', session_id=filename2)
+    return render_template('deepLearning/dlembedding/embedding.html', session_id=filename2,
+    description=description['dlembedding_embedding'], tooltip_dimension=tooltips['dimension'], 
+    tooltip_model_dropdown=tooltips['model_dropdown'], tooltip_layout=tooltips['layout_dropdown'], 
+    tooltip_features=tooltips['features_checkbox'])
